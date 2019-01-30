5509e77cfac5ab9334e5f67a399b5392300ea085
Logan Yu <yu.logan@gmail.com>
2019-01-30 15:36:18 +0300

    create app

---
+++ frontend/rollcall.jsx
@@ -0,0 +1,30 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import configureStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  // we don't put the store directly on the window because
+  // it can be confusing when debugging, sometimes giving you access to state
+  // when you shouldn't
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
