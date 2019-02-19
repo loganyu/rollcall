ba6c1b721ce84bab235dfa1831003a1faaf6093b
Logan Yu <yu.logan@gmail.com>
2019-02-19 13:44:57 -0600

    styling for event creation

--- frontend/components/event_form/event_form.jsx
+++ frontend/components/event_form/event_form.jsx
@@ -12,10 +12,10 @@ class EventForm extends React.Component {
     const description = new URLSearchParams(location.search).get('description') || '';
     const start_time = new URLSearchParams(location.search).get('start_time') || '';
     const event_repeat = new URLSearchParams(location.search).get('event_repeat') || '';
-    const weekly_interval = new URLSearchParams(location.search).get('weekly_interval') || '';
-    const weekly_day_of_week = new URLSearchParams(location.search).get('weekly_day_of_week') || '';
-    const monthly_day_of_week = new URLSearchParams(location.search).get('monthly_day_of_week') || '';
-    const week_of_month = new URLSearchParams(location.search).get('week_of_month') || '';
+    const weekly_interval = new URLSearchParams(location.search).get('weekly_interval') || '1';
+    const weekly_day_of_week = new URLSearchParams(location.search).get('weekly_day_of_week') || '1';
+    const monthly_day_of_week = new URLSearchParams(location.search).get('monthly_day_of_week') || '1';
+    const week_of_month = new URLSearchParams(location.search).get('week_of_month') || '1';
 
     
     this.state = {
@@ -76,155 +76,161 @@ class EventForm extends React.Component {
 
     return (
       <div className="new-event-container">
-        <div className="new-event-form">
-          <h3 className="new-event-title">Event</h3>
+        <form className="new-event-form" onSubmit={this.handleSubmit}>
+          <h2 className="new-event-title">New Event</h2>
+          <label htmlFor="name">Name</label>
+          <input
+            required
+            id="name"
+            type="text"
+            value={name}
+            onChange={this.update('name')}
+            className="event-field"
+          />
+          <br/>
 
-          <form onSubmit={this.handleSubmit}>
-            <label className="event-field">Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={this.update('name')}
-              className="event-field"
-            />
-            <br/>
+          <label htmlFor="date">Date</label>
+          <input
+            required
+            id="date"
+            type="date"
+            value={date}
+            onChange={this.update('date')}
+            className="event-field"
+          />
+          <br />
 
-            <label className="event-field">Date</label>
-            <input
-              type="date"
-              value={date}
-              onChange={this.update('date')}
-              className="event-field"
-            />
-            <br />
+          <label htmlFor="start_time">Start Time</label>
+          <input
+            required
+            id="start_time"
+            type="time"
+            value={start_time}
+            onChange={this.update('start_time')}
+            className="event-field"
+          />
+          <br />
 
-            <label className="address-field">Address</label>
-            <input
-              type="text"
-              value={address}
-              onChange={this.update('address')}
-              className="event-field"
-              required
-            />
-            <br />
+          <label htmlFor="address">Address</label>
+          <input
+            required
+            id="address"
+            type="text"
+            value={address}
+            onChange={this.update('address')}
+            className="event-field"
+          />
+          <br />
 
-            <label className="event-field">Description</label>
-            <br/>
-            <textarea
-              value={description}
-              onChange={this.update('description')}
-              className="event-field"
-            />
-            <br />
+          <label htmlFor="description">Description</label>
+          <textarea
+            required
+            id="description"
+            value={description}
+            onChange={this.update('description')}
+            className="event-field"
+          />
+          <br />
 
-            <hr />
+          <label htmlFor="event-repeat">How often does this event repeat?</label>
+          <br />
+          <select 
+            id="event-repeat"
+            onChange={this.update('event_repeat')}
+            defaultValue={event_repeat}
+          >
+            <option value="">Only Once</option>
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+            <option value="monthly">Monthly</option>
+          </select>
+          <br />
 
-            <label className="event-field">Start Time</label>
-            <input
-              type="time"
-              value={start_time}
-              onChange={this.update('start_time')}
-              className="event-field"
-            />
-            <br />
+          {this.state.event_repeat === "weekly" && 
+            <div>
+              <label htmlFor="weekly-interval">Repeat Every</label>
+              <select
+                id="weekly-interval"
+                onChange={this.update('weekly_interval')}
+                defaultValue={weekly_interval}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+              week(s)
+              <br />
 
-            <label className="event-field">How often does this event repeat?</label>
-            <select 
-              onChange={this.update('event_repeat')}
-              defaultValue={event_repeat}
-            >
-              <option value="">Only Once</option>
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="monthly">Monthly</option>
-            </select>
-            <br />
-
-            {this.state.event_repeat === "weekly" && 
-              <div>
-                <label className="event-field">Repeat Every</label>
-                <input
-                  type="text"
-                  value={weekly_interval}
-                  onChange={this.update('weekly_interval')}
-                  className="event-field"
-                />
-                week(s)
-                <br />
-
-                <label className="event-field">Day of Week</label>
-                <select 
-                  onChange={this.update('weekly_day_of_week')}
-                  defaultValue={weekly_day_of_week}
-                >
-                  <option value=""></option>
-                  <option value="1">Monday</option>
-                  <option value="2">Tuesday</option>
-                  <option value="3">Wednesday</option>
-                  <option value="4">Thursday</option>
-                  <option value="5">Friday</option>
-                  <option value="6">Saturday</option>
-                  <option value="7">Sunday</option>
-                </select>
-                <br />
-              </div>
-            }
-
-            {this.state.event_repeat === "monthly" && 
-              <div>
-                <label className="event-field">Repeat Every</label>
-                <select 
-                  onChange={this.update('week_of_month')}
-                  defaultValue={week_of_month}
-                >
-                  <option value=""></option>
-                  <option value="1">1st</option>
-                  <option value="2">2nd</option>
-                  <option value="3">3rd</option>
-                  <option value="4">4th</option>
-                  <option value="-1">Last</option>
-                </select>
-                Week
-                <br/>
-
-                <label className="event-field">Day of Week</label>
-                <select 
-                  onChange={this.update('monthly_day_of_week')}
-                  defaultValue={monthly_day_of_week}
-                >
-                  <option value=""></option>
-                  <option value="1">Monday</option>
-                  <option value="2">Tuesday</option>
-                  <option value="3">Wednesday</option>
-                  <option value="4">Thursday</option>
-                  <option value="5">Friday</option>
-                  <option value="6">Saturday</option>
-                  <option value="0">Sunday</option>
-                </select>
-                <br />
-              </div>
-            }
-
-            <hr />
-
-            <div className="button-holder">
-              <input
-                type="submit"
-                value="Submit"
-                className="new-event-button"
-              />
+              <label htmlFor="weekly-day-of-week">Day of Week</label>
+              <select 
+                id="weekly-day-of-week"
+                onChange={this.update('weekly_day_of_week')}
+                defaultValue={weekly_day_of_week}
+              >
+                <option value="1">Monday</option>
+                <option value="2">Tuesday</option>
+                <option value="3">Wednesday</option>
+                <option value="4">Thursday</option>
+                <option value="5">Friday</option>
+                <option value="6">Saturday</option>
+                <option value="7">Sunday</option>
+              </select>
+              <br />
             </div>
-          </form>
+          }
 
-          <div className="button-holder">
+          {this.state.event_repeat === "monthly" && 
+            <div>
+              <label htmlFor="week-of-month">Repeat Every</label>
+              <select 
+                id="week-of-month"
+                onChange={this.update('week_of_month')}
+                defaultValue={week_of_month}
+              >
+                <option value="1">1st</option>
+                <option value="2">2nd</option>
+                <option value="3">3rd</option>
+                <option value="4">4th</option>
+                <option value="-1">Last</option>
+              </select>
+              Week
+              <br/>
+
+              <label htmlFor="monthly-day-of-week">Day of Week</label>
+              <select 
+                id="monthly-day-of-week"
+                onChange={this.update('monthly_day_of_week')}
+                defaultValue={monthly_day_of_week}
+              >
+                <option value=""></option>
+                <option value="1">Monday</option>
+                <option value="2">Tuesday</option>
+                <option value="3">Wednesday</option>
+                <option value="4">Thursday</option>
+                <option value="5">Friday</option>
+                <option value="6">Saturday</option>
+                <option value="0">Sunday</option>
+              </select>
+              <br />
+            </div>
+          }
+
+          <div className='create-event-buttons'>
             <button
-              className="new-event-button"
+              type="submit"
+              className="create-event-button"
+            >
+              Create Event
+            </button>
+            <button
+              className="gray-button"
               onClick={this.props.history.goBack}
             >
               Cancel
             </button>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
