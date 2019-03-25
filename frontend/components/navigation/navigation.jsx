91ada72014e898c71cb3aee56be1d19a2af33926
Logan Yu <yu.logan@gmail.com>
2019-03-24 22:25:43 -0400

    start add app bar

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { connect } from 'react-redux';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
 
 class Navigation extends React.Component { 
   constructor(props) {
@@ -29,47 +52,60 @@ class Navigation extends React.Component {
     const { currentUser } = this.props;
 
     return (
-      <header className="main-nav">
-        <nav className="left-nav">
-          <ul>
-            <li>
-              <a href="#">Runspots</a>
-            </li>
-          </ul>
-        </nav>
-        <nav className="right-nav">
-          <ul>
-            <li>
-              <div
-                className="new-club-link"
-                onClick={this.handleCreateClub}>
-                Start a new club
-            </div>
-            </li>
-            <li>
-              <Link to="/">Clubs</Link>
-            </li>
-            {currentUser &&
-              <li>
-                <a onClick={this.handleLogOut}>Log Out</a>
-              </li>
-            }
-            {!currentUser &&
-              <li>
-                <Link to="/login">Log in</Link>
-              </li>
-            }
-            {!currentUser &&
-              <li>
-                <Link to="/signup">Sign up</Link>
-              </li>
-            }
-          </ul>
-        </nav>
-      </header>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      // <header className="main-nav">
+      //   <nav className="left-nav">
+      //     <ul>
+      //       <li>
+      //         <a href="#">Runspots</a>
+      //       </li>
+      //     </ul>
+      //   </nav>
+      //   <nav className="right-nav">
+      //     <ul>
+      //       <li>
+      //         <div
+      //           className="new-club-link"
+      //           onClick={this.handleCreateClub}>
+      //           Start a new club
+      //       </div>
+      //       </li>
+      //       <li>
+      //         <Link to="/">Clubs</Link>
+      //       </li>
+      //       {currentUser &&
+      //         <li>
+      //           <a onClick={this.handleLogOut}>Log Out</a>
+      //         </li>
+      //       }
+      //       {!currentUser &&
+      //         <li>
+      //           <Link to="/login">Log in</Link>
+      //         </li>
+      //       }
+      //       {!currentUser &&
+      //         <li>
+      //           <Link to="/signup">Sign up</Link>
+      //         </li>
+      //       }
+      //     </ul>
+      //   </nav>
+      // </header>
     )
   }
 };
 
 
-export default withRouter(Navigation);
+export default withRouter(connect()(withStyles(styles)(Navigation)))
